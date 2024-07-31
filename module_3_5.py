1ceef24691d522bd66d335feaf5dc28c3a22f8eb
ShumsAn <shum-sol@outlook.com>
2024-07-31 13:52:33 +0300

    Задача "Рекурсивное умножение цифр"
    Самостоятельная работа по уроку "Рекурсия"

--- module_3_5.py
+++ module_3_5.py
@@ -3,13 +3,9 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
 
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:])), print(f'{first}X{int(str_number[1:])}')
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
-
-
-
-
 
 
 result = get_multiplied_digits(40203)
